51d11e1f210fc31a912fddbb05bf2fd602e4e769
mj <jss7802@gmail.com>
2016-06-03 19:56:48 +0900

    안개 아이템 추가

---
+++ TetrisForDesktop/TetrisForDesktop_F/src/TetrisView.h
@@ -0,0 +1,31 @@
+#ifndef _TETRIS_VIEW_H
+#define _TETRIS_VIEW_H
+
+#include "TetrisManager.h"
+#include "RankingManager.h"
+
+typedef struct _tetrisView{
+	TetrisManager tetrisManager;
+	RankingManager rankingManager;
+	int level;
+	int mainMenu;
+	int pauseMenu;
+	int endMenu;
+}TetrisView;
+
+void TetrisView_StartGame(TetrisView* tetrisView);
+void TetrisView_ProcessGame(TetrisView* tetrisView, int processType, int direction, int isSplash); //// isSplash 매개변수 추가
+void TetrisView_PauseGame(TetrisView* tetrisView);
+void TetrisView_EndGame(TetrisView* tetrisView);
+void TetrisView_ShowRanking(TetrisView* tetrisView);
+void TetrisView_AddRanking(TetrisView* tetrisView);
+void TetrisView_ShowSetting(TetrisView* tetrisView);
+void TetrisView_ProcessMainMenu(TetrisView* tetrisView);
+void TetrisView_ProcessPauseMenu(TetrisView* tetrisView);
+void TetrisView_ProcessEndMenu(TetrisView* tetrisView);
+
+//make temporarily for calling from main function
+DWORD TetrisView_GetDownMilliSecond(TetrisView* tetrisView);
+void TetrisView_MakeHold(TetrisView* tetrisView);
+
+#endif
